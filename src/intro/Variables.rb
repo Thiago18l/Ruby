5cde8d5ea63133f89e1a11f3e44b520dc62be419
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-10-14 20:59:41 -0300

    Variables and casting for values

---
+++ src/intro/Variables.rb
@@ -0,0 +1,24 @@
+
+# constants names are declared by beginning the variable name with a capital letter
+VALUE = "Values"
+
+puts VALUE
+
+#Ruby and Variable Dynamic type
+a = 10
+puts a
+a = 25
+puts a # now "a" is 25.
+
+
+## Converting Variable values
+y = 20
+puts y.to_f
+
+# for binary
+number_to_convert = 1997
+puts number_to_convert.to_s(2)
+
+# hexadecimal or octal
+puts number_to_convert.to_s(8)
+puts number_to_convert.to_s(16)
